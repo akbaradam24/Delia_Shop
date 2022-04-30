05591c153973ccab643ccf3fcf89d57dc586284b
akbaradam24 <adam.akhbar24@gmail.com>
2022-04-30 20:51:48 +0700

    add redux thunk

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,9 +6,26 @@ function Home() {
     <div>
       <Navbar />
       <div className="homeContainer">
-        <h1>Welcome to Your Data</h1>
+        <h1>Welcome to Delia Shop</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi animi iure vel quam fuga laborum, doloribus dolores ullam ducimus aliquid asperiores id voluptatum, quas earum vero optio dicta, molestias placeat?</p>
         <br />
+        <table>
+          <tr>
+            <th>Product</th>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+          <tr>
+            <td>Book</td>
+            <td>How to train your amet</td>
+            <td>Rp.99.000</td>
+          </tr>
+          <tr>
+            <td>Medicine</td>
+            <td>Paracetamol</td>
+            <td>Rp.23.000</td>
+          </tr>
+        </table>
 
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis consequuntur incidunt dolore cum? Ex maxime, dignissimos saepe doloremque alias minus ratione nam excepturi ipsa illo atque repellendus dicta dolor laudantium.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi animi iure vel quam fuga laborum, doloribus dolores ullam ducimus aliquid asperiores id voluptatum, quas earum vero optio dicta, molestias placeat?</p>
